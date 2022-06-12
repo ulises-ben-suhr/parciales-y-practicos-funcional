4e2ca66a336d230daf9f7388f15ee0f50899da3c
LaSudestada <ulisessuhr@hotmail.com>
2022-06-12 11:25:44 -0300

    parcial-Thanos-Completo

---
+++ Escuelita-de-Thanos/thanosNew.hs
@@ -0,0 +1,94 @@
+-- PRIMERA PARTE
+
+data Guantelete = Guantelete {
+    material :: String,
+    gemas :: [Gema]
+} 
+
+type Gema = Personaje -> Personaje
+
+data Personaje = Personaje {
+    nombre :: String,
+    edad :: Int,
+    energia :: Int,
+    planetaResidencia :: String,
+    habilidades :: [String]
+} deriving(Show, Eq)
+
+data Universo = Universo {
+    habitantes :: [Personaje]
+} deriving(Show, Eq)
+
+-- PUNTO 1
+
+chasquido :: Guantelete -> Universo -> Universo
+chasquido guantelete universo 
+    | esDelInfinito guantelete =  universo {habitantes = (take (((`div` 2) . length . habitantes) universo) . habitantes) universo }
+    | otherwise = universo
+
+esDelInfinito :: Guantelete -> Bool
+esDelInfinito guantelete = ((== "uru") . material) guantelete && ((== 6) . length . gemas) guantelete
+
+-- PUNTO 2
+
+aptoPendex :: Universo -> Bool
+aptoPendex = any ((<45) . edad) . habitantes
+
+energiaUniversal :: Universo -> Int
+energiaUniversal = sum . map energia . filter ((>1) . length . habilidades) . habitantes
+
+-- SEGUNDA PARTE
+
+-- PUNTO 3
+
+quitarEnergia :: Int -> Personaje -> Personaje
+quitarEnergia valor personaje = personaje {energia = energia personaje - valor}
+
+quitarHabilidad :: String -> Personaje -> Personaje
+quitarHabilidad habilidad personaje = personaje {habilidades = (filter (/= habilidad). habilidades) personaje}
+
+teletransportar :: String -> Personaje -> Personaje
+teletransportar planeta personaje = personaje {planetaResidencia = planeta}
+
+rejuvenecer :: Int -> Personaje -> Personaje
+rejuvenecer anios personaje = personaje {edad = max 18 (edad personaje - anios)}
+
+
+
+
+laMente :: Int -> Personaje -> Personaje
+laMente energiaPerdida = quitarEnergia energiaPerdida
+
+elAlma :: String -> Personaje -> Personaje
+elAlma habilidadPerdida enemigo 
+    | ( elem habilidadPerdida . habilidades ) enemigo = (quitarEnergia 10 . quitarHabilidad habilidadPerdida) enemigo
+    | otherwise = quitarEnergia 10 enemigo
+
+elEspacio :: String -> Personaje -> Personaje
+elEspacio planeta = quitarEnergia 20 . teletransportar planeta
+
+elPoder :: Personaje -> Personaje
+elPoder enemigo 
+    | ((<=2) . length . habilidades) enemigo = enemigo {energia = 0, habilidades = []}
+    | otherwise = enemigo {energia = 0} 
+
+elTiempo :: Personaje -> Personaje
+elTiempo enemigo | ((>18) . edad) enemigo = (quitarEnergia 50 . rejuvenecer (((`div` 2) . edad) enemigo)) enemigo
+                 | otherwise = quitarEnergia 50 enemigo
+
+laLocura :: Gema -> Personaje -> Personaje
+laLocura gema = gema . gema
+
+-- PUNTO 4 y 5
+
+manopla :: Guantelete
+manopla = Guantelete "goma" [elTiempo, elAlma "usar mjolnir", laLocura (elAlma "programacion en Haskell")]
+
+ulises :: Personaje
+ulises = Personaje "ulises" 24 60 "tierra" ["programacion en Haskell", "usar mjolnir", "cocinar"]
+
+atacar :: Guantelete -> Personaje -> Personaje
+atacar guantelete personajeInicial = foldl (\pers efecto -> efecto pers ) personajeInicial (gemas guantelete)
+
+-- PUNTO 6
+
---
+++ Pinky y Cerebro/pyc.hs
@@ -0,0 +1,89 @@
+data Animal = Animal {
+    intelecto :: Int,
+    especie :: String,
+    capacidades :: [String]
+} deriving(Show,Eq)
+
+cerebro :: Animal
+cerebro = Animal 150 "raton" ["estratega"]
+pinky :: Animal
+pinky = Animal 115 "raton" ["espia","hacer narf","hacer asda","hacer guau"]
+
+
+inteligenciaSuperior :: Int -> Animal -> Animal
+inteligenciaSuperior n bicho = bicho { intelecto = intelecto bicho + n }
+
+pinkificar :: Animal -> Animal
+pinkificar bicho = bicho { capacidades = [] }
+
+superpoderes :: Animal -> Animal
+superpoderes bicho | especie bicho == "elefante" = bicho { capacidades = capacidades bicho ++ ["no tenerle miedo a los ratones"] }
+                   | especie bicho == "raton" && intelecto bicho > 100 = bicho { capacidades = capacidades bicho ++ ["hablar"] }
+                   | otherwise = bicho
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+
+antropomorficos :: Animal -> Bool
+antropomorficos = (\ b -> ((> 60) . intelecto) b && (elem "hablar" .capacidades) b)
+
+noTanCuerdo :: Animal -> Bool
+noTanCuerdo = (>2) . length . filter pinkiesco . capacidades
+
+pinkiesco :: String -> Bool
+pinkiesco habilidad = ((== "hacer") . take 5) habilidad && ((\p -> ((<=4) . length) p && any esVocal p) . drop 6) habilidad
+
+esVocal :: Char -> Bool
+esVocal letra = letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+
+type Transformacion = Animal -> Animal
+
+data Experimento = Experimento {
+    pruebas :: [Transformacion],
+    criterioExito ::  Animal -> Bool
+}
+
+torturas1 :: Experimento
+torturas1 = Experimento [pinkificar,inteligenciaSuperior 10, superpoderes] antropomorficos
+
+experimentoExitoso :: Experimento -> Animal -> Bool
+experimentoExitoso experimento bicho =  criterioExito experimento ((flip aplicarPruebas bicho . pruebas) experimento)
+
+aplicarPruebas ::  [Transformacion] -> Animal -> Animal
+aplicarPruebas torturas bicho = foldl (\animalito prueba -> prueba $ animalito) bicho torturas
+
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+
+reporte :: [String] -> [Transformacion] -> ([String] -> [String] -> Bool) -> [Animal] -> [Animal]
+reporte habilidades experimento cuantificador = filter (cuantificador habilidades . capacidades) . map (aplicarPruebas experimento)
+
+reporteIntelectual :: [Animal] -> [String] -> [Transformacion] -> [Int]
+reporteIntelectual victimas habilidades experimento = (map intelecto . reporte habilidades experimento hayRepetidos) victimas
+
+reporteTaxonomico :: [Animal] -> [String] -> [Transformacion] -> [String]
+reporteTaxonomico victimas habilidades experimento = (map especie . reporte habilidades experimento todosRepetidos) victimas
+
+reporteCapacitivo :: [Animal] -> [String] -> [Transformacion] -> [[String]]
+reporteCapacitivo victimas habilidades experimento = (map capacidades . reporte habilidades experimento noHayRepetidos) victimas
+
+noHayRepetidos :: Eq a => [a] -> [a] -> Bool
+noHayRepetidos lista = not . hayRepetidos lista
+
+hayRepetidos :: Eq a => [a] -> [a] -> Bool
+hayRepetidos lista = any (\v -> elem v lista)
+
+todosRepetidos :: Eq a => [a] -> [a] -> Bool
+todosRepetidos lista = all (\v -> elem v lista)
+
+-- lista1 = ["hola", "como", "estas", "yo", "mas", "o", "menos"]
+-- lista2 = ["problema", "mio", "chau"]
+-- lista3 = ["yo", "ando", "mejor", "que", "la", "vez", "anterior", "chau"]
+-- lista4 = ["nada", "mal"]
+-- lista5 = ["nada", "mal"]
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+
+
